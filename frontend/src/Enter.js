1e9d128c6ca1c15c134f67613372d5f66c5f1a81
chenjoachim <lllion66666@gmail.com>
2023-03-06 08:12:14 +0800

    add some easter egg & party

--- frontend/src/Enter.js
+++ frontend/src/Enter.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import party from "party-js";
 import { CreateWord } from "./Memorizer";
 const Enter = ({ onClose }) => {
   const [eng, setEng] = useState("");
   const [chi, setChi] = useState("");
-  const handleSubmit = () => {
+  const handleSubmit = (but) => {
     console.log({ eng: eng, chi: chi });
     CreateWord(eng, chi)
       .then((response) => {
         if (response.type === "error") throw response.mes;
-        else {
+        else if (
+          eng === "3/11" ||
+          eng === "anniversary" ||
+          chi === "紀念日" ||
+          chi === "交往紀念日"
+        ) {
+          party.confetti(but, {
+            count: party.variation.range(20, 90),
+          });
+        } else {
           alert(
             response.mes.type + ' word "' + response.mes.word + '" success!'
           );
@@ -21,6 +31,7 @@ const Enter = ({ onClose }) => {
   const handleQuit = () => {
     onClose();
   };
+
   return (
     <>
       <p>Enter a word and its meaning:</p>
@@ -34,7 +45,7 @@ const Enter = ({ onClose }) => {
         onChange={(e) => setChi(e.target.value)}
         placeholder="Chinese"
       ></input>
-      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={(e) => handleSubmit(e.target)}>Submit</button>
       <button onClick={handleQuit}>Go To Test</button>
     </>
   );
